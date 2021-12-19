c1ba8eeb9d1abdd144d7d822ad5c32407678eec9
devil-k <ehdgur0812@gmail.com>
2021-12-19 15:58:04 +0900

    add jump code

--- DH_Tutorial/Assets/Scripts/MoveController.cs
+++ DH_Tutorial/Assets/Scripts/MoveController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class MoveController : MonoBehaviour
 {
     public float moveSpeed = 10f;
+    public float jumpPower = 300f;
 
     Vector3 moveVelocity;
+    Rigidbody2D rigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
+        rigidBody = transform.GetComponent<Rigidbody2D>();
+
         moveVelocity = Vector3.zero;
 
         transform.position = Vector3.zero;
@@ -20,12 +24,21 @@ public class MoveController : MonoBehaviour
     void Update()
     {
         moveVelocity.Set(Input.GetAxis("Horizontal"),
-                         Input.GetAxis("Vertical"),
+                         0f,
                          0f);
         moveVelocity = moveVelocity * Time.deltaTime * moveSpeed;
 
-        Debug.Log(moveVelocity);
-
         transform.Translate(moveVelocity);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rigidBody.velocity = Vector2.zero;
+        rigidBody.AddForce(Vector2.up * jumpPower);
     }
 }
